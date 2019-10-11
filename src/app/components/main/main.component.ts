f65be0b1a29e0425cf2bc9fddabdf2f337a52bc3
Gustavo Miranda <gustavoa@kyros.com.br>
2019-10-11 09:06:27 -0300

    first2

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, ChangeDetectionStrategy } from "@angular/core";
 import { UsersService } from "src/app/services/users.service";
 import { HttpParams } from "@angular/common/http";
-import { PageEvent } from "@angular/material/paginator";
 
 @Component({
   selector: "app-main",
@@ -22,15 +21,18 @@ export class MainComponent implements OnInit {
     id: 0,
     last_name: ""
   };
-
+  length = 100;
+  pageSize = 5;
+  pageSizeOptions: number[] = [5];
   constructor(private userService: UsersService) {}
 
   ngOnInit() {
-    this.users = JSON.parse(localStorage.getItem("user"));
-    if (this.users.length === 0) {
-      this.find(1);
-      localStorage.removeItem("users");
-      localStorage.setItem("users", JSON.stringify(this.users));
+    this.find(1);
+    let usersStorage = JSON.parse(localStorage.getItem("user"));
+    if(usersStorage.length !== undefined){
+      if(usersStorage.length !== this.users.length){
+       this.users = usersStorage;
+      }
     }
   }
   find(page) {
@@ -46,16 +48,12 @@ export class MainComponent implements OnInit {
   getUsers(event: any): void {
     this.find(event.pageIndex + 1);
   }
-  length = 100;
-  pageSize = 5;
-  pageSizeOptions: number[] = [5];
-  pageEvent: PageEvent;
+
 
   setPageSizeOptions(setPageSizeOptionsInput: string) {
     this.pageSizeOptions = setPageSizeOptionsInput.split(",").map(str => +str);
   }
   inserirUsuario() {
-    console.log(this.users);
     this.user.id = this.length + 1;
     this.users = [...this.users, this.user];
     localStorage.removeItem("users");
